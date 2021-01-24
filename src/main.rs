091c97077c0e43dfa19fc63707205f6b94585c4e
Filip <lazoviccorp@gmail.com>
2021-01-24 22:48:29 +0100

    atm 2

--- src/main.rs
+++ src/main.rs
@@ -5,46 +5,29 @@ mod clean_up;
 mod matrix;
 mod plotter;
 mod simulation;
-mod vector;
 
-use matrix::Matrix;
+use matrix::{find_constants, general_solution, run};
 use plotter::plot;
-use vector::Vector;
 
 fn main() {
-	simulation::start();
-	let v1 = Vector::from(vec![-0.2, 0.0, -2.0059707967319333]);
-	let v2 = Vector::from(vec![-2.1061890623329234, -0.1, 0.0]);
-	let v3 = Vector::from(vec![0.0, -2.093908522219756, -0.1]);
+	let v1 = vec![-0.2, 0.0, -2.0059707967319333];
+	let v2 = vec![-2.1061890623329234, -0.1, 0.0];
+	let v3 = vec![0.0, -2.093908522219756, -0.1];
 
-	let m = Matrix::from(vec![v1, v2, v3]);
-
-	let consts = m.find_constants(vec![0.1, 0.1, 0.2]);
-	println!("consts: {:?}", consts);
-	let ampl = m.amplitude(consts.clone());
-	println!("amplitudes: {:?}", ampl);
-	let mut pts1 = Vec::new();
-	let mut pts2 = Vec::new();
-	let mut pts3 = Vec::new();
-	for i in 0..200 {
-		let t = i as f64;
-		let res = m.run(&consts, t);
-		pts1.push((t, res[0].re + res[0].im));
-		pts2.push((t, res[1].re + res[1].im));
-		pts3.push((t, res[2].re + res[2].im));
-	}
-	plot(&[pts1, pts2, pts3], 3);
-
-	// let mut xs = Vec::new();
-	// let mut ys = Vec::new();
-	// let mut x = 0.1;
-	// let mut y = 0.1;
-	// for j in 0..200 {
-	// 	let t = j as f64;
-	// 	x += y;
-	// 	y += -0.1 * x;
-	// 	xs.push((t, x));
-	// 	ys.push((t, y));
+	let m = vec![v1, v2, v3];
+	let sol = general_solution(m.clone(), 0.0);
+	let consts = find_constants(sol, vec![0.1, 0.1, 0.2]);
+	let res = run(m.clone(), &consts, 1000.0);
+	println!("res: {:?}", res);
+	// let mut pts1 = Vec::new();
+	// let mut pts2 = Vec::new();
+	// let mut pts3 = Vec::new();
+	// for i in 0..600 {
+	// 	let t = i as f64 / 10.0;
+	// 	let res = run(m.clone(), &consts, t);
+	// 	pts1.push((t, res[0]));
+	// 	pts2.push((t, res[1]));
+	// 	pts3.push((t, res[2]));
 	// }
-	// plot(&[xs, ys], 4);
+	// plot(&[pts1, pts2, pts3], 3);
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,118 +1,139 @@
-use crate::vector::Vector;
 use ndarray::*;
 use ndarray_linalg::*;
 use num_complex::Complex;
-use std::convert::From;
-use std::ops::Index;
 
-pub struct Matrix {
-	value: Vec<Vector>,
-	rows: usize,
-	cols: usize,
+type MatrixComplex = Vec<Vec<Complex<f64>>>;
+type Matrix = Vec<Vec<f64>>;
+type VectorComplex = Vec<Complex<f64>>;
+type Vector = Vec<f64>;
+
+const width: usize = 3;
+
+pub fn matrix_to_vec(matrix: Matrix) -> Vec<f64> {
+	let mut mx = Vec::new();
+	for i in 0..width {
+		for j in 0..width {
+			mx.push(matrix[i][j]);
+		}
+	}
+	mx
 }
 
-impl From<Vec<Vector>> for Matrix {
-	fn from(a: Vec<Vector>) -> Self {
-		Self {
-			value: a.to_vec(),
-			rows: a.len(),
-			cols: a.len(),
-		}
+pub fn product_vec(c: f64, vector: &Vector) -> Vector {
+	let mut new_vec = Vec::new();
+	for r in vector {
+		new_vec.push(c * r);
 	}
+	new_vec
 }
 
-impl Matrix {
-	pub fn to_vec(&self) -> Vec<f64> {
-		let mut mx = Vec::new();
-		for i in 0..self.rows {
-			for j in 0..self.cols {
-				mx.push(self.value[i][j].re);
-			}
-		}
-		mx
+pub fn add_vec(lv: &Vector, rv: &Vector) -> Vector {
+	let mut new_vec = Vec::new();
+	for i in 0..rv.len() {
+		new_vec.push(lv[i] + rv[i]);
 	}
-
-	pub fn eigen(&self) -> (Vector, Vec<Vector>) {
-		let mx = self.to_vec();
-		let a = Array::from_shape_vec((self.rows, self.cols), mx).unwrap();
-		let (evals, evecs) = a.eig().unwrap();
-
-		let mut new_evals = vec![Complex::new(0.0, 0.0); self.rows];
-		for i in 0..self.rows {
-			new_evals[i].re = evals[i].re;
-			new_evals[i].im = evals[i].im;
-		}
-
-		let raw_vec = evecs.into_raw_vec();
-		let mut new_evecs = vec![Vector::zeros(self.rows); self.cols];
-		for i in 0..self.rows {
-			for j in 0..self.cols {
-				let index = i * self.rows + j;
-				new_evecs[i][j].re = raw_vec[index].re;
-				new_evecs[i][j].im = raw_vec[index].im;
-			}
-		}
-		(Vector::from(new_evals), new_evecs)
-	}
-
-	pub fn general_solution(&self, t: f64) -> Matrix {
-		let (eigenvalues, eigenvectors) = self.eigen();
-
-		let mut res = Vec::new();
-		for i in 0..self.rows {
-			res.push((eigenvalues[i] * t).exp() * eigenvectors[i].clone());
-		}
-
-		Matrix::from(res)
-	}
-
-	pub fn amplitude(&self, consts: Vec<f64>) -> Vec<f64> {
-		let t = 1.;
-		let (eigenvalues, eigenvectors) = self.eigen();
-
-		let mut res = Vector::zeros(self.rows);
-		for i in 0..eigenvalues.len() {
-			if eigenvalues[i].im != 0.0 {
-				res += consts[i] * (eigenvalues[i] * t).exp() * eigenvectors[i].clone();
-			}
-		}
-		let mut m = Vec::new();
-		for c in res.iter() {
-			m.push((c.re.powf(2.0) + c.im.powf(2.0)).sqrt());
-		}
-		m
-	}
-
-	pub fn find_constants(&self, res: Vec<f64>) -> Vec<f64> {
-		let t = 0.0;
-		let sol = self.general_solution(t);
-		let mut entries = Vec::new();
-		for i in 0..self.rows {
-			for j in 0..self.cols {
-				let c = sol[j][i];
-				entries.push((i, j, c.re + c.im));
-			}
-		}
-		let mut m = sparse21::Matrix::from_entries(entries);
-
-		let soln = m.solve(res).unwrap();
-		soln
-	}
-
-	pub fn run(&self, consts: &Vec<f64>, t: f64) -> Vector {
-		let sol = self.general_solution(t);
-		let mut res = Vector::zeros(self.rows);
-		for i in 0..consts.len() {
-			res += consts[i] * sol[i].clone();
-		}
-		res
-	}
+	new_vec
 }
 
-impl Index<usize> for Matrix {
-	type Output = Vector;
-
-	fn index(&self, idx: usize) -> &Vector {
-		&self.value[idx]
+pub fn sub_vec(lv: &Vector, rv: &Vector) -> Vector {
+	let mut new_vec = Vec::new();
+	for i in 0..rv.len() {
+		new_vec.push(lv[i] - rv[i]);
 	}
+	new_vec
+}
+
+pub fn get_bs(vec: &VectorComplex) -> (Vector, Vector) {
+	let mut re = Vec::new();
+	let mut im = Vec::new();
+	for c in vec {
+		re.push(c.re);
+		im.push(c.im);
+	}
+	(re, im)
+}
+
+pub fn eigen(matrix: Matrix) -> (VectorComplex, MatrixComplex) {
+	let mx = matrix_to_vec(matrix);
+	let a = Array::from_shape_vec((width, width), mx).unwrap();
+	let (evals, evecs) = a.eig().unwrap();
+
+	let mut new_evals = vec![Complex::new(0.0, 0.0); width];
+	for i in 0..width {
+		new_evals[i].re = evals[i].re;
+		new_evals[i].im = evals[i].im;
+	}
+
+	let raw_vec = evecs.into_raw_vec();
+	let cz = Complex::<f64>::from(0.0);
+	let mut new_evecs = vec![vec![cz; width]; width];
+	for i in 0..width {
+		for j in 0..width {
+			let index = i * width + j;
+			new_evecs[i][j].re = raw_vec[index].re;
+			new_evecs[i][j].im = raw_vec[index].im;
+		}
+	}
+	(new_evals, new_evecs)
+}
+
+pub fn general_solution(m: Matrix, t: f64) -> Matrix {
+	let (eigenvalues, eigenvectors) = eigen(m);
+	for ev in eigenvalues.clone() {
+		println!("{}", ev);
+	}
+	for evec in eigenvectors.clone() {
+		println!("{:?}", evec);
+	}
+
+	let e1 = (eigenvalues[2].re * t).exp();
+	let (bc, _) = get_bs(&eigenvectors[2]);
+
+	let e2 = (eigenvalues[1] * t).exp();
+	let (a, b) = (e2.re, e2.im);
+	let (b1, b2) = get_bs(&eigenvectors[1]);
+
+	let cos1 = product_vec(a, &b1);
+	let sin1 = product_vec(b, &b2);
+	let cos2 = product_vec(a, &b2);
+	let sin2 = product_vec(b, &b1);
+
+	let x0 = product_vec(e1, &bc);
+	let x1 = sub_vec(&cos1, &sin1);
+	let x2 = add_vec(&cos2, &sin2);
+
+	vec![x0, x1, x2]
+}
+
+pub fn find_constants(m: Matrix, res: Vec<f64>) -> Vec<f64> {
+	let t = 0.0;
+	let sol = general_solution(m, t);
+	let x0 = sol[0].clone();
+	let x1 = sol[1].clone();
+	let x2 = sol[2].clone();
+	let entries = vec![
+		(0, 0, x0[0]),
+		(0, 1, x1[0]),
+		(0, 2, x2[0]),
+		(1, 0, x0[1]),
+		(1, 1, x1[1]),
+		(1, 2, x2[1]),
+		(2, 0, x0[2]),
+		(2, 1, x1[2]),
+		(2, 2, x2[2]),
+	];
+	let mut m = sparse21::Matrix::from_entries(entries);
+
+	let soln = m.solve(res).unwrap();
+	soln
+}
+
+pub fn run(m: Matrix, consts: &Vec<f64>, t: f64) -> Vector {
+	let sol = general_solution(m, t);
+	let mut res = vec![0.0; width];
+	for i in 0..width {
+		let pr = product_vec(consts[i], &sol[i].clone());
+		res = add_vec(&res, &pr);
+	}
+	res
 }
